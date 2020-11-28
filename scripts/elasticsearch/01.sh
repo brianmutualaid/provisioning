533a3c8f783b1082656d3a2e6c73d2ad52df670a
Brian <brian@mutualaid.info>
2020-11-28 15:18:53 -0500

    es

---
+++ scripts/elasticsearch/01.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Mostly from https://www.elastic.co/guide/en/elasticsearch/reference/7.10/rpm.html
+
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+
+cat << EOF > /etc/yum.repos.d/elasticsearch.repo
+[elasticsearch]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=0
+autorefresh=1
+type=rpm-md
+EOF
+
+dnf install --enablerepo=elasticsearch elasticsearch
+
+/bin/systemctl daemon-reload
+/bin/systemctl enable elasticsearch.service
+systemctl start elasticsearch.service
+
